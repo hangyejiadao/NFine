596eae38634dc607ee289cff4e042bdbf2324c93
liaohang <953917332@qq.com>
2018-07-22 23:09:56 +0800

    adf

---
+++ NFine/NFine.Data/DbContext/NFineDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFine.Data.DbContext
+{
+    public class NFineDbContextFactory
+    {
+        private NFineDbContext Instance;
+
+        public NFineDbContext CreateInstance()
+        {
+            //Microsoft.EntityFrameworkCore.DbContext dbContext = (Microsoft.EntityFrameworkCore.DbContext)CallContext.GetData("dbContext");
+            //if (dbContext == null)
+            //{
+            //    dbContext = new NFineDbContext();
+            //    CallContext.SetData("dbContext", dbContext);
+            //}
+            //return dbContext;
+            return new NFineDbContext();
+        }
+
+    }
+}
